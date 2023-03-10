8d343eabf6bcae21bc418202f5c61c682934d4e5
Norbert Horvath <nhorvathkzgimi@gmail.com>
2023-03-10 10:02:19 +0100

    Add project files.

---
+++ API/CLILibrary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CLILibrary
+{
+    enum Types
+    {
+        BOOL, STRING, INT, LONG, FLOAT, DOUBLE
+    }
+    class Tester 
+    {
+        public void testerfunc() 
+        {
+            Console.WriteLine("test working");
+        }
+    }
+    class Parser
+    {
+        string delimiter;
+    }
+    class CommandLine
+    {
+        readonly List<Command> commands;
+
+    }
+    abstract class Command
+    {
+        readonly List<Parameter> options;
+        string Name { get; set; }
+        string Description { get; set; }
+        public abstract void Run();
+
+    }
+    class Parameter
+    {
+        public string ShortName { get; set; }
+        public string LongName { get; set; }
+        public List<Types> supported = new();
+        public bool IsOptional { get; set; }
+        public Parameter(string shortname, string longname)
+        {
+            ShortName = shortname;
+            LongName = longname;
+        }
+        public void Run<T>(T value)
+        {
+
+        }
+
+
+    }
+}
---
+++ API/Tester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using CLILibrary;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var tester = new Tester();
+            tester.testerfunc();
+        }
+    }
+}
