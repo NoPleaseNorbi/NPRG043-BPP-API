a5529f66aa3a8a48bf6d325e0a8721eb201f4e65
Norbert Horvath <nhorvathkzgimi@gmail.com>
2023-03-10 12:07:04 +0100

    Fixed warnings

--- API/CLILibrary.cs
+++ API/CLILibrary.cs
@@ -13,30 +13,30 @@ namespace CLILibrary
 
     public class Option
     {
-        public string ShortName { get; set; }
-        public string LongName { get; set; }
-        public string Description { get; set; }
+        public string? ShortName { get; set; }
+        public string? LongName { get; set; }
+        public string? Description { get; set; }
         public OptionType Type { get; set; }
-        public object DefaultValue { get; set; }
-        public object Value { get; set; }
-        public List<string> Synonyms { get; set; }
-        public bool IsRequired { get; set; }
-        public bool IsFlag { get; set; }
-        public bool IsSet { get; set; }
-        public List<string> AllowedValues { get; set; }
-        public int MinValue { get; set; }
-        public int MaxValue { get; set; }
+        public object? DefaultValue { get; set; }
+        public object? Value { get; set; }
+        public List<string>? Synonyms { get; set; }
+        public bool? IsRequired { get; set; }
+        public bool? IsFlag { get; set; }
+        public bool? IsSet { get; set; }
+        public List<string>? AllowedValues { get; set; }
+        public int? MinValue { get; set; }
+        public int? MaxValue { get; set; }
     }
 
     public class CommandLineParser
     {
-        public List<string> PlainArguments { get; set; }
-        public List<Option> Options { get; set; }
-        public List<Option> RequiredOptions { get; set; }
-        public string Usage { get; set; }
-        public string Version { get; set; }
+        public List<string>? PlainArguments { get; set; }
+        public List<Option>? Options { get; set; }
+        public List<Option>? RequiredOptions { get; set; }
+        public string? Usage { get; set; }
+        public string? Version { get; set; }
         
-
+    
         public void AddOption(Option option) 
         {
             Console.WriteLine("Not Implemented");
